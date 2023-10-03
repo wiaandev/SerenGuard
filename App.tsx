b4fc42fce5293af859283402c90b173142b6bea2
wiaandev <wiaanduvenhage.dev@gmail.com>
2023-10-04 00:12:26 +0200

    added image recognition with google cloud

--- App.tsx
+++ App.tsx
@@ -1,47 +1,51 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
 import ImageTest from "./src/screens/ImageTest";
+import { PaperProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   //loading the fonts onto the app
-  const [fontsLoaded] = useFonts({
-    epilogueRegular: require("./assets/fonts/Epilogue-Regular.ttf"),
-    epilogueBold: require("./assets/fonts/Epilogue-Bold.ttf"),
-    caveat: require("./assets/fonts/Caveat-Regular.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  // epilogueRegular: require("./assets/fonts/Epilogue-Regular.ttf"),
+  // });
 
   // UseEffect for splash screen
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-    prepare();
-  }, []);
+  // useEffect(() => {
+  //   async function prepare() {
+  //     await SplashScreen.preventAutoHideAsync();
+  //   }
+  //   prepare();
+  // }, []);
 
-  //if check for the fonts
-  if (!fontsLoaded) {
-    return undefined;
-  } else {
-    SplashScreen.hideAsync();
-  }
+  // //if check for the fonts
+  // if (!fontsLoaded) {
+  //   return undefined;
+  // } else {
+  //   SplashScreen.hideAsync();
+  // }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="ImageTest"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={ImageTest} />
-        {/* Add more screens here */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="ImageTest"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="ImageTest" component={ImageTest} />
+            {/* Add more screens here */}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 };
 
@@ -50,3 +54,5 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+export default App;
--- src/screens/ImageTest.tsx
+++ src/screens/ImageTest.tsx
@@ -1,10 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacityComponent,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { apiKey } from "../../config/api";
 
 export default function ImageTest() {
+  const [imageUri, setImageUri] = useState<string | null>(null);
+  const [labels, setLabels] = useState([]);
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const analyzeImage = async () => {
+    try {
+      if (!imageUri) {
+        alert("Please select an image!");
+        return;
+      }
+
+      //API Key
+      const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+
+      //read the image file and convert to base64
+      const base64ImageData = await FileSystem.readAsStringAsync(imageUri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      const requestData = {
+        requests: [
+          {
+            image: {
+              content: base64ImageData,
+            },
+            features: [{ type: "LABEL_DETECTION", maxResults: 5 }],
+          },
+        ],
+      };
+
+      const apiResponse = await axios.post(apiUrl, requestData);
+      setLabels(apiResponse.data.responses[0].labelAnnotations);
+    } catch (error) {
+      console.log(error);
+      alert("Error analyzing image. Please try again later.");
+    }
+  };
   return (
-    <View>
-      <Text>ImageTest</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <Text>This is an image being tested...</Text>
+
+      {imageUri && (
+        <Image source={{ uri: imageUri }} style={{ width: 300, height: 300 }} />
+      )}
+
+      <TouchableOpacity onPress={pickImage}>
+        <Text>Pick an Image</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={analyzeImage}>
+        <Text>Analyze it</Text>
+      </TouchableOpacity>
+      {
+        labels.length > 0 && (
+            <View>
+                <Text>Labels: </Text>
+                {
+                    labels.map((label) => (
+                        <Text key={label.mid}>
+                            {label.description}
+                        </Text>
+                    ))
+                }
+            </View>
+        )
+      }
+    </SafeAreaView>
+  );
 }
