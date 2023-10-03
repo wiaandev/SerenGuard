6cb92ad77cd63e80a974ea0c3bd10d33cec11743
wiaandev <wiaanduvenhage.dev@gmail.com>
2023-10-03 23:10:20 +0200

    added dependencies + modified app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+import ImageTest from "./src/screens/ImageTest";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  //loading the fonts onto the app
+  const [fontsLoaded] = useFonts({
+    epilogueRegular: require("./assets/fonts/Epilogue-Regular.ttf"),
+    epilogueBold: require("./assets/fonts/Epilogue-Bold.ttf"),
+    caveat: require("./assets/fonts/Caveat-Regular.ttf"),
+  });
+
+  // UseEffect for splash screen
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  //if check for the fonts
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ImageTest"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={ImageTest} />
+        {/* Add more screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
